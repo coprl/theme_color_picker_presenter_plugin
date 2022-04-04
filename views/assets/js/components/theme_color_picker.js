b962579c28246647f79c42a272380a4e3b7b9012
Rhet Prieto <rhetprieto@gmail.com>
2022-04-04 09:02:37 -0600

    Create ThemeColorPicker logic

---
+++ views/assets/js/components/theme_color_picker.js
@@ -0,0 +1,60 @@
+class ThemeColorPicker {
+  constructor(component) {
+    this.component = component;
+    this.data = {};
+    this.colorBlock = this.component.querySelector('.v-color-picker');
+    this.colorInput = this.component.querySelector('.v-color-input')
+    this.picker = this.createPicker();
+    this.colorInput.addEventListener('change', this.colorInputHandler());
+  }
+
+  createPicker() {
+    return new Picker({
+      parent: this.colorBlock,
+      popup: 'top',
+      color: this.colorInput.value,
+      editorFormat: 'hex',
+      alpha: false,
+      editor: true,
+      onOpen: () => {
+        toggleUploaderIndex("0");
+      },
+      onClose: () => {
+        toggleUploaderIndex("1");
+      },
+      onChange: (color) => {
+        this.colorBlock.style.backgroundColor = color.hex;
+        this.colorInput.value = color.hex.slice(0,-2);
+        const previewSectionId = this.component.dataset['previewSectionId'];
+
+        if (previewSectionId) {
+          const parsedPreviewVars = JSON.parse(this.component.dataset['previewVars']);
+          updateColorVars(color.hex, previewSectionId, parsedPreviewVars)
+        }
+      },
+   });
+  }
+
+
+  colorInputHandler () {
+    const self = this;
+    return function(event){
+      let color = event.target.value;
+      self.picker.setColor(color)
+    }
+  }
+}
+
+function toggleUploaderIndex(zIndex){
+  const uploadElement = document.getElementsByName("upload_file");
+  if (uploadElement && uploadElement[0]) {
+    uploadElement[0].style.zIndex = zIndex;
+  }
+}
+
+function updateColorVars(color, previewSectionId, previewVars) {
+  const previewSection = document.getElementById(previewSectionId)
+  previewVars.forEach( (varName) => {
+    previewSection.style.setProperty('--' + varName, color);
+  })
+}
